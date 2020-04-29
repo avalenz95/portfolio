750d3b3ba0d93ac5db5dd93332c978fc055318ad
ablades <audaris@gmail.com>
2020-04-29 11:27:40 -0700

    add icons to nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,19 +6,43 @@ function Nav() {
             <div className="navbar-end">
             <div className="navbar-item">
                 <div className="field is-grouped">
-                <p className="control">
-                <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.8.2/bulma-0.8.2.zip">
-        
-                </a>
-                </p>
-                <p className="control">
-                    <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.8.2/bulma-0.8.2.zip">
-                    <span className="icon">
-                    <i className="fab fa-github"></i>
-                    </span>
-                    <span>Download</span>
-                    </a>
-                </p>
+
+                    <p className="control">
+                        <a className="button is-primary" href="https:www.medium.com/ablades">
+                        <span className="icon">
+                            <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/medium-icon.svg`} alt="github"/>
+                        </span>
+                        <span>Medium</span>
+                        </a>
+                    </p>
+
+                    <p className="control">
+                        <a className="button is-primary" href="https:www.medium.com/ablades">
+                        <span className="icon">
+                            <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/linkedin-icon.svg`} alt="github"/>
+                        </span>
+                        <span>Linkedin</span>
+                        </a>
+                    </p>
+
+                    <p className="control">
+                        <a className="button is-primary" href="https:www.github.com/ablades">
+                        <span className="icon">
+                            <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/github-icon.svg`} alt="github"/>
+                        </span>
+                        <span>Github</span>
+                        </a>
+                    </p>
+
+                    {/* ADD DOWNLOAD AND RESUME */}
+                    <p className="control">
+                        <a className="button is-primary" href="https:www.medium.com/ablades">
+                        <span className="icon">
+                            <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/resume-icon.svg`} alt="github"/>
+                        </span>
+                        <span>Resume</span>
+                        </a>
+                    </p>
                 </div>
             </div>
             </div>

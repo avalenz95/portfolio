cf7fe6b819061ee2ca9ce856593eb75ca2e2a0cb
ablades <audaris@gmail.com>
2020-04-28 22:47:19 -0700

    dashboard and panel skeleton

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Represents homepage grid
+function Dashboard() {
+    return (
+        <div className="Dashboard">
+
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/Panel/Panel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Represents a panel block on dashboard (about me, projects, articles)
+function Panel() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Panel

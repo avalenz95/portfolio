dfb12034eed12e6065fc1de9fddb3393df24d793
ablades <audaris@gmail.com>
2020-05-05 20:47:18 -0700

    about component transitions work

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import Heading from '../Heading/Heading.js'
 import './About.css'
+import { motion } from "framer-motion";
+
+
+const transition = {
+    duration: 1,
+    ease: [0.43, 0.13, 0.23, 0.96]
+  };
+  
+  const variants = {
+    exit: { y: "50%", opacity: 0, transition },
+    enter: {
+      y: "0%",
+      opacity: 1,
+      transition
+    }
+  };
+  
 
 function About() {
 
@@ -28,65 +45,67 @@ function About() {
     return (
 
         <div className="Dashboard container">
-            <div className="box" style={box}>
-                <Heading name="About Me"/>
+            <motion.div className="single" initial="exit" animate="enter" exit="exit" variants={variants}>
+                <div className="box" style={box}>
+                    <Heading name="About Me"/>
 
-                <div class="columns">
-                    <div class="box" style={section}>
-                        <div class="column is-three-quarters">
-                            <p class="title is-3" style={text} >
-                                My Background
-                            </p>
-                            
-                            <p class="subtitle is-5" style={text}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                            </p>
-                        </div>
+                    <div class="columns">
+                        <div class="box" style={section}>
+                            <div class="column is-three-quarters">
+                                <p class="title is-3" style={text} >
+                                    My Background
+                                </p>
+                                
+                                <p class="subtitle is-5" style={text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                </p>
+                            </div>
 
-                        <div class="column is-one-quarter" style={text}>
+                            <div class="column is-one-quarter" style={text}>
 
-                        </div>
-                    </div>
-                </div>
-                <br></br>
-                <div class="columns">
-                    <div class="box" style={section}>
-                        <div class="column is-three-quarters">  
-                            <p class="subtitle is-5" style={text}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                            </p>
-
-                        </div>
-
-                        <div class="column is-one-quarter">
-                            <p class="title is-3" style={text}>
-                                My Experience
-                            </p>
+                            </div>
                         </div>
                     </div>
+                    <br></br>
+                    <div class="columns">
+                        <div class="box" style={section}>
+                            <div class="column is-three-quarters">  
+                                <p class="subtitle is-5" style={text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                </p>
 
-                </div>
-                <br></br>
-                <div class="columns">
-                    <div class="box" style={section}>
-                        <div class="column is-three-quarters">
-                            <p class="title is-3" style={text}>
-                                My Ambitions
-                            </p>
-                            
-                            <p class="subtitle is-5" style={text}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                            </p>
+                            </div>
+
+                            <div class="column is-one-quarter">
+                                <p class="title is-3" style={text}>
+                                    My Experience
+                                </p>
+                            </div>
                         </div>
 
-                        <div class="column is-one-quarter" style={text}>
+                    </div>
+                    <br></br>
+                    <div class="columns">
+                        <div class="box" style={section}>
+                            <div class="column is-three-quarters">
+                                <p class="title is-3" style={text}>
+                                    My Ambitions
+                                </p>
+                                
+                                <p class="subtitle is-5" style={text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                </p>
+                            </div>
 
+                            <div class="column is-one-quarter" style={text}>
+
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </div>
+                </div>
+            </motion.div>
         </div>
     )
 }
--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,6 +1,17 @@
 import React from 'react'
 //import Badge from '../Badge/Badge.js'
 import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
+
+const transition = {
+    duration: 1,
+    ease: [0.43, 0.13, 0.23, 0.96]
+}
+
+const backVariants = {
+    exit: { x: 100, opacity: 0, transition },
+    enter: { x: 0, opacity: 1, transition: { delay: 1, ...transition } }
+}
 
 function Heading(props) {
     const {name} = props
@@ -28,13 +39,15 @@ function Heading(props) {
                     </h1>
                 </div>
                 <div className="column">
-                    <Link to="/" style={style.button}>
-                        <button className="button is-large" style={style.button} href="#">
-                            <span className="icon is-large">
-                                <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/chevron-back-icon.svg`} alt="return"/>
-                            </span>
-                        </button>
-                    </Link>
+                    <motion.div className="back" variants={backVariants}>
+                        <Link to="/" style={style.button}>
+                            <button className="button is-large" style={style.button} href="#">
+                                <span className="icon is-large">
+                                    <img className="is-rounded" src={`${process.env.PUBLIC_URL}icons/chevron-back-icon.svg`} alt="return"/>
+                                </span>
+                            </button>
+                        </Link>
+                    </motion.div>
                 </div>
         </div>
     )
